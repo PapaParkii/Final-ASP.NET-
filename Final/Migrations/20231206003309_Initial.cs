919e72aa22bdc17e860028bb2c60aaa86e60f5be
Parker <ptown@DESKTOP-4C33OP4>
2023-12-05 18:35:32 -0600

    Migrations

---
+++ Final/Migrations/20231206003309_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Final.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Monsters",
+                columns: table => new
+                {
+                    MonsterID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Monsters", x => x.MonsterID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Monsters",
+                columns: new[] { "MonsterID", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Spider", 2 },
+                    { 2, "Bracken", 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Monsters");
+        }
+    }
+}
